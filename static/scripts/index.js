0b6a7ab8fe6a4d434594485afe155e01c392ccf6
Marek ‘saji’ Augustynowicz <marek.augustynowicz@verkstedt.com>
2021-06-03 01:39:23 +0200

    EsLint errors

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,14 +1,15 @@
-const players = new Map()
+const players = new Map();
 
 const STARTING_LIFE = 20;
-const MESSAGE_HANG_TIME_MS = 2000; // FIXME Hang longer for accessibility?
+// FIXME Hang longer for accessibility?
+const MESSAGE_HANG_TIME_MS = 2000;
 
 function handleStepperClick (event)
 {
     const stepper = event.target;
-    const playerElement = stepper.closest('[data-player]')
-    const player = players.get(playerElement.dataset.player)
-    const step = parseInt(stepper.dataset.step)
+    const playerElement = stepper.closest('[data-player]');
+    const player = players.get(playerElement.dataset.player);
+    const step = parseInt(stepper.dataset.step, 10);
 
     // FIXME Misbehaves on non–touch
     player.playerElement.dataset.buttonFocusWithin = 'true';
@@ -18,11 +19,13 @@ function handleStepperClick (event)
 
     player.lifeInput.valueAsNumber += step;
 
-    const change = parseInt(player.messageElement.dataset.change || 0) + step;
+    const change =
+        parseInt(player.messageElement.dataset.change || 0, 10) + step;
     player.messageElement.dataset.change = change;
     player.messageElement.hidden = false;
     player.messageElement.textContent = change > 0 ? `+${change}` : change;
     player.messageElement.dataset.incoming = 'true';
+    // eslint-disable-next-line no-unused-expressions
     player.messageElement.offsetHeight;
     player.messageElement.dataset.incoming = 'false';
     clearTimeout(player.messageTimeoutId);
@@ -31,15 +34,16 @@ function handleStepperClick (event)
             player.messageElement.dataset.change = 0;
             player.messageElement.hidden = true;
         },
-        MESSAGE_HANG_TIME_MS
-    )
+        MESSAGE_HANG_TIME_MS,
+    );
 }
 
 function drawPlayer ()
 {
     const playerIds = Array.from(players.keys());
-    const playerId = playerIds[Math.floor(Math.random() * playerIds.length)];
-    const player = players.get(playerId)
+    const playerId =
+        playerIds[Math.floor(Math.random() * playerIds.length)];
+    const player = players.get(playerId);
 
     player.playerElement.dataset.chosen = 'true';
     setTimeout(() => { // FIXME Find a better way
@@ -51,12 +55,13 @@ async function main ()
 {
     await navigator.wakeLock.request('screen');
 
-    if (window.location.hostname !== 'localhost' && !window.location.hostname.endsWith('.loca.lt')) {
+    if (window.location.hostname !== 'localhost' && !window.location.hostname.endsWith('.loca.lt'))
+    {
         navigator.serviceWorker.register('./serviceWorker.js');
     }
 
-    document.querySelectorAll('button[data-step]').forEach(stepper => {
-        const playerElement = stepper.closest('[data-player]')
+    document.querySelectorAll('button[data-step]').forEach((stepper) => {
+        const playerElement = stepper.closest('[data-player]');
         const data = {
             playerElement,
             lifeInput: playerElement.querySelector('[data-component=life]'),
@@ -66,19 +71,22 @@ async function main ()
         players.set(playerElement.dataset.player, data);
 
         stepper.addEventListener('click', handleStepperClick);
+        // eslint-disable-next-line no-param-reassign
         stepper.hidden = false;
-    })
+    });
 
     document.querySelector('button[type=reset]').addEventListener('click', (event) => {
         event.preventDefault();
 
-        players.forEach(player => {
+        players.forEach((player) => {
+            // eslint-disable-next-line no-param-reassign
             player.lifeInput.valueAsNumber = STARTING_LIFE;
+            // eslint-disable-next-line no-param-reassign
             player.messageElement.hidden = true;
             clearTimeout(player.lifeInput.messageTimeoutId);
         });
         drawPlayer();
-    })
+    });
 
     // TODO Click player name to edit it, but also change bg colour
 
